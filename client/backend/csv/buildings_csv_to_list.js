5e1e51716c1f4e7bf6be0386716fbb3c557dcd8d
Jaret McManus <jaret.mcmanus@stonybrook.edu>
2025-03-10 11:35:47 -0400

    created cleaned buildings csv

--- client/backend/csv/buildings_csv_to_list.js
+++ client/backend/csv/buildings_csv_to_list.js
@@ -26,7 +26,49 @@ function parseBuildings(){
       }); 
     })
 }
+function writeCleanBuildingsToCSV(buildings){
+  const header = Object.keys(buildings[0]);
+  let csvRows = [];
+  csvRows.push(header.join(','));
+
+  for(const row of buildings){
+    const values = header.map(key => {
+      let value = row[key];
+      value = value.replace('“', "");
+      value = value.replace('”', "");
+      value = value.replace('"', "");
+
+      return value;
+    });
+    csvRows.push(values.join(','));
+  }
+  
+  csvRows = csvRows.join('\n');
+  fs.writeFile('csv/buildings_data.csv', csvRows, (err) => {
+    if(err){
+      console.error('Error csv');
+    }else{
+      console.log("Cleaned csv saved.\n");
+    }
+  });
+}
 (async () => {
-  const buildings = await parseBuildings();
-  console.log('first', buildings[0]);
+  let buildings = await parseBuildings();
+  
+  //filter out buildings off campus
+  const on_campus = ['SBU SOUTH', 'SBU EAST', 'SBU WEST', 'SBU R&D'];
+  let off_campus = [];
+  buildings = buildings.filter(building_data => {
+    if( on_campus.includes(building_data['campus']) ){
+      return true;
+    } else {
+      if( !off_campus.includes(building_data['campus']) ){
+        off_campus.push(building_data['campus']);
+      }
+      return false;
+    }
+  });
+  console.log(`off_campus=${off_campus}`);
+
+  writeCleanBuildingsToCSV(buildings);
 })();
