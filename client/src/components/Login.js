93433c180e29564f4e2fe7133e9903a32d829d83
dane-meister <dane.meister@stonybrook.edu>
2025-03-30 12:54:12 -0400

    Login and register is WORKING

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import axios from 'axios';
 import '../stylesheets/Auth.css';
 
 export default function LoginPage() {
